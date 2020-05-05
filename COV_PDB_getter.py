e0bbb2801480de7d5f6bb3b357074a648de4439f
ellinkle <64062717+ellinkle@users.noreply.github.com>
2020-05-05 13:48:36 +1000

    Update COV_PDB_getter.py

--- COV_PDB_getter.py
+++ COV_PDB_getter.py
@@ -1,12 +1,26 @@
-import pypdb
+import requests
 
-pdb_file = pypdb.get_pdb_file('6W9Q', filetype='cif', compression=False)
-print(pdb_file[:200])
+string = '_diffrn_detector.pdbx_collection_date'
+
+
+def cif_downloader(ID):
+    full_url = "https://files.rcsb.org/download/"
+    full_url += ID + ".cif"
+    response = requests.get(full_url)
+    open(f"{ID}.cif", 'wb').write(response.content)
+
 
 def lines_that_contain(string, fp):
     return [line for line in fp if string in line]
 
-string = '_diffrn_detector.pdbx_collection_date'
-result = lines_that_contain(string, pdb_file)
-date = str(result).split(' ')
-print(date)
+
+def collection_date(cif_file):
+    result = lines_that_contain(string, pdb_file)
+    date = str(result).split(' ')[9]
+    return(date)
+
+# example!
+PDB_ID = '3FY7'
+
+cif_downloader(PDB_ID)
+print(collection_date(PDB_ID))
